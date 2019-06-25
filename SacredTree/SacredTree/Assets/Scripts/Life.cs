ef0257159af1bbdc75c22136bde2dd7731cfb2ea
Rycher <Rycherwilliam@hotmail.com>
2019-06-25 20:55:06 -0300

    FCommit

---
+++ SacredTree/SacredTree/Assets/Scripts/Life.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Life : MonoBehaviour
+{
+    public static float Health;
+    float MaxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 100f;
+        Health = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Image HealthBar = GameObject.Find("HealthBar").GetComponent<Image>();
+        HealthBar.fillAmount = Health / MaxHealth;
+    }
+}
---
+++ SacredTree/SacredTree/Assets/Scripts/UI_Play.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UI_Play : MonoBehaviour    
+{
+    public Button Play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Play.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        //Button Play = transform.Find("btnPlay").GetComponent<Button>();
+                     
+    }
+
+    
+    void TaskOnClick()
+    {
+        Debug.Log("Clicado");
+        SceneManager.LoadScene(1);        
+    }
+}
